3a2f23d7578f0ae17a1d52da6aa78c3cf8d29d75
andrade-felipe-dev <83104193+andrade-felipe-dev@users.noreply.github.com>
2022-12-06 20:35:22 -0300

    init: first commit
    Create a project and a first writing test and a create a api

---
+++ tests/Feature/TodoListTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\TodoList;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TodoListTest extends TestCase
+{
+    use RefreshDatabase;
+    public function test_store_todo_list()
+    {
+        //preparetion // prepare
+        TodoList::create(['name' => 'my list']);
+        //action // perform
+        $response = $this->getJson(route('todo-list.store'));
+        //assertion //predict
+        $this->assertEquals(1, count($response->json()));
+    }
+}
