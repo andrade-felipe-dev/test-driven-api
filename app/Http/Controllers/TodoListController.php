cc1dd170cb0a805156c5f46957eee3a141aff1c7
andrade-felipe-dev <83104193+andrade-felipe-dev@users.noreply.github.com>
2022-12-07 21:55:56 -0300

    testes unitários
    Adicionado teste e função sobre o todo

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -4,13 +4,43 @@ namespace App\Http\Controllers;
 
 use App\Models\TodoList;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class TodoListController extends Controller
 {
-    public function index()
+    public function index(): mixed
     {
         $todo = TodoList::all();
 
         return response($todo);
+    }
+
+    public function show(TodoList $list)
+    {
+        return response($list);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required'
+        ]);
+        $list = TodoList::create($request->all());
+
+        return response($list, Response::HTTP_CREATED);
+    }
+
+    public function destroy(TodoList $list)
+    {
+        $list->delete();
+
+        return response('', Response::HTTP_NO_CONTENT);
+    }
+
+    public function update(Request $request, TodoList $list)
+    {
+        $list->update($request->all());
+
+        return $list;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,14 @@
 use App\Http\Controllers\TodoListController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('todo-list', [TodoListController::class, 'index'])->name('todo-list.store');
+
+
+
+
+Route::prefix('todo-list')->group(function () {
+  Route::get('', [TodoListController::class, 'index'])->name('todo-list.index');
+  Route::get('/{list}', [TodoListController::class, 'show'])->name('todo-list.show');
+  Route::post('', [TodoListController::class, 'store'])->name('todo-list.store');
+  Route::delete('/{list}', [TodoListController::class, 'destroy'])->name('todo-list.destroy');
+  Route::patch('/{list}', [TodoListController::class, 'update'])->name('todo-list.update');
+});
