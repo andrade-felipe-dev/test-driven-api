640505669ea4df477002b9fc22f5ddb9e3e571d3
andrade-felipe-dev <83104193+andrade-felipe-dev@users.noreply.github.com>
2022-12-16 23:35:48 -0300

    feat: tests and functions
    Add a store, index and a delete methods with task

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,9 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use App\Http\Controllers\TodoListController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::apiResource('todo-list', TodoListController::class);
-
-// Route::prefix('todo-list')->group(function () {
-//   Route::get('', [TodoListController::class, 'index'])->name('todo-list.index');
-//   Route::get('/{list}', [TodoListController::class, 'show'])->name('todo-list.show');
-//   Route::post('', [TodoListController::class, 'store'])->name('todo-list.store');
-//   Route::delete('/{list}', [TodoListController::class, 'destroy'])->name('todo-list.destroy');
-//   Route::patch('/{list}', [TodoListController::class, 'update'])->name('todo-list.update');
-// });
+Route::apiResource('task', TaskController::class);
